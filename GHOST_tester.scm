581a9570f142b52914d9c6542707eab0852d9fa5
Kirubel Kassaye <joejnke@gmail.com>
2018-10-12 17:56:56 +0300

    avoid E

--- GHOST_tester.scm
+++ GHOST_tester.scm
@@ -1,80 +1,138 @@
-;;; Testing code for checking integrity of the notebook
+;;; Testing code for checking integrity of the notebook ;;;
 
 ; The tests to be checked need to be listed in tests.txt in the following format.
-; 1. Every line must start with  R corresponding to a rule expression,
-;                                M corresponding to an expression with expected output,
-;                                E corresponding to an expected output for the expression defined in an immediate previous line with M,
+; 1. Every line must start with  R corresponding to an expression with expected output,
+;                                M corresponding to an expected output for the expression defined in an immediate previous line with R,
 ;                                N corresponding to an expression with no expected output.
 ; 2. After the above symbol, a space (or any number of space) is required before writing rules or expression.
-; 3. Every rule must have the label "lbl" for internal use.
-; 4. Every rule must have ^keep function for testing the same rule with multiple inputs.
-; 5. Every expression must be contained in a single line.
+; 3. Expression with multiple lines is allowed
+; 4. Comment lines are allowed
+; 5. Lines with comments along side an expression are not allowed.
+;    for example:
+;                 (define animal "dog") ; variable definition
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-modules (ice-9 textual-ports))
 (use-modules (texinfo string-utils))
-
 (use-modules (srfi srfi-64))
-(use-modules (opencog)
-             (opencog nlp)
-             (opencog nlp relex2logic)
-             (opencog openpsi)
-             (opencog ghost)
-             (opencog ghost procedures))
 
 (define (test-func)
 	(call-with-input-file "tests.txt"
 		(lambda (port)
-			(define num-rules 0)
-      (define num-frules 0)
-			(define num-M 0)
-      (define num-fM 0)
+      (define num-R 0)
+      (define num-fR 0)
       (define num-N 0)
       (define num-fN 0)
       (define num-skpln 0)
 			(define rule "")
 			(define rule-atom '())
       (define expected "")
+      (define temp-line "")
 
 			(while #t
 				(let ((line (get-line port)) (type "") (res '()))
 					(if (eof-object? line) (break))
 					(set! line (string-trim line))
-					(display (format #f "-> ~a\n"line))
 
-					(set! type (string-ref line 0))
+          ; escape empty lines in the file
+          (if (string-null? line)
+            (continue)
+          )
+
+          (set! type (string-ref line 0))
+          ; escape comment lines in the file
+          (if (equal? type #\;)
+            (continue)
+          )
+
 					(set! line (string-trim (substring line 1)))
 
+          ; take care of multiple line command or expected value as a single entity
+          ;(if (not-equal? type #\M or #\N or #\R) (begin (set! line (string-join (list line temp-line))) (continue)) (set! line temp-line) )
+
+          (set! temp-line (get-line port))
+          (if (not (eof-object? temp-line))
+            (begin
+              (set! temp-line (string-trim temp-line))
+              ; for escaping empty lines by considering as comment line
+              (if (string-null? temp-line)
+                (set! temp-line (string #\;))
+              )
+              (if (and (not (equal? (string-ref temp-line 0) #\R)) (not (equal? (string-ref temp-line 0) #\M)) (not (equal? (string-ref temp-line 0) #\N)))
+                (begin
+                  (while (and (not (equal? (string-ref temp-line 0) #\R)) (not (equal? (string-ref temp-line 0) #\M)) (not (equal? (string-ref temp-line 0) #\N)))
+                    ; escape comment lines in the file
+                    (if (not (equal? (string-ref temp-line 0) #\;))
+                      (set! line (string-join (list line temp-line)))
+                    )
+
+                    (set! temp-line (get-line port))
+                    (if (eof-object? temp-line) (break))
+                    (set! temp-line (string-trim temp-line))
+                    ; for escaping empty lines by considering as comment line
+                    (if (string-null? temp-line)
+                      (set! temp-line (string #\;))
+                    )
+                  )
+                  (if (not (eof-object? temp-line)) (unread-string (string-append temp-line (string #\lf)) port)) ;return the reading point one line back.
+                )
+                (if (not (eof-object? temp-line)) (unread-string (string-append temp-line (string #\lf)) port));return the reading point one line back.
+              )
+            )
+          )
+
+          (display (format #f "-> ~a\n" (string-join (list (string type) line))))
 					(cond
 					((equal? type #\R)
 						(begin
-            (catch #t
-              (lambda () (display (test-read-eval-string line)))
-              (lambda (key . args)
-                (display "\nERROR: possibly syntax error.\n")
-                (set! num-frules (+ num-frules 1))
-              )
-            )
-						(display "\n\n")
-						(set! num-rules (+ num-rules 1))
-						(set! rule line)
-						))
-					((equal? type #\M)
-						(begin
-						(set! num-M (+ num-M 1))
+						(set! num-R (+ num-R 1))
 
             ;;; CHECK FOR EXPECTED OUTPUT
-            ; check if the next line begins with E and if it does compare the output of the line with M
-            ; to this line and if it doesn't begin with N raise error and return the get-line position back again.
+            ; check if the next line begins with M and if it does compare the output of the line with R
+            ; to this line and if it doesn't begin with M raise error and return the get-line position back again.
+
             (catch #t
-              (lambda () (set! expected (string-trim (get-line port))))
+              (lambda ()
+                (set! expected (string-trim (get-line port)))
+              )
               (lambda (key . args)
-                (display "\nERROR: end of file") ; handles if M is the last statement in the file.
+                (display "\nERROR: Error in test file format \n       Skipped current line...\n")
                 (set! expected "non")
               )
             )
 
-            (if (equal? (string-ref expected 0) #\E)
+            (if (equal? (string-ref expected 0) #\M)
                 (begin
+                  (set! temp-line (get-line port))
+                  ; for escaping empty lines by considering as comment line
+                  (if (string-null? temp-line)
+                    (set! temp-line (string #\;))
+                  )
+                  (if (not (eof-object? temp-line))
+                    (begin
+                      (set! temp-line (string-trim temp-line))
+                      (if (and (not (equal? (string-ref temp-line 0) #\R)) (not (equal? (string-ref temp-line 0) #\M)) (not (equal? (string-ref temp-line 0) #\N)))
+                        (begin
+                          (while (and (not (equal? (string-ref temp-line 0) #\R)) (not (equal? (string-ref temp-line 0) #\M)) (not (equal? (string-ref temp-line 0) #\N)))
+                            ; escape comment lines in the file
+                            (if (not (equal? (string-ref temp-line 0) #\;))
+                              (set! expected (string-join (list expected temp-line)))
+                            )
+                            (set! temp-line (get-line port))
+                            (if (eof-object? temp-line) (break))
+                            (set! temp-line (string-trim temp-line))
+                            ; for escaping empty lines by considering as comment line
+                            (if (string-null? temp-line)
+                              (set! temp-line (string #\;))
+                            )
+                          )
+                          (if (not (eof-object? temp-line)) (unread-string (string-append temp-line (string #\lf)) port)) ;return the reading point one line back.
+                        )
+                        (if (not (eof-object? temp-line)) (unread-string (string-append temp-line (string #\lf)) port));return the reading point one line back.
+                      )
+                    )
+                  )
+
                   (catch #t
                     (lambda ()
                       (set! expected (string-trim (substring expected 1)))
@@ -86,23 +144,23 @@
                     )
                     (lambda (key . args)
                       (display "\nERROR: possibly syntax error.\n")
-                      (set! num-fM (+ num-fM 1))
+                      (set! num-fR (+ num-fR 1))
                     )
                   )
 
                 )
                 (begin
                   (display "\nERROR: Missing Expected Statement\n")
-                  (set! num-fM (+ num-fM 1))
+                  (set! num-fR (+ num-fR 1))
                   (unread-string (string-append expected (string #\lf)) port) ;return the reading point one line back.
                 )
             )
 						)
           )
 
-          ((equal? type #\E)
+          ((equal? type #\M)
           (begin
-						(display "\nERROR: Missing preceding match (M) statement \n       Skipped current line...\n")
+						(display "\nERROR: Missing preceding Command with Expected output (R) statement \n       Skipped current line...\n")
 						(set! num-skpln (+ num-skpln 1))
 						)
           )
@@ -119,7 +177,7 @@
                 (set! num-fN (+ num-fN 1))
               )
             )
-            (display "\n")
+            (display "\n\n")
 						)
           )
 
@@ -133,11 +191,10 @@
 				)
 			)
 
-    (display (format #f "\n\n# of total executions: ~a\n" (+ num-rules num-M num-N)))
-    (display (format #f "# of failed rule executions: ~a\n" num-frules))
-    (display (format #f "# of failed non-rule executions: ~a\n" (+ num-fM num-fN)))
+    (display (format #f "\n\n# of total executions: ~a\n" (+ num-R num-N)))
+    (display (format #f "# of failed executions: ~a\n" (+ num-fR num-fN)))
     (display (format #f "# of Skipped lines: ~a\n" num-skpln))
-    (display (format #f "# of tested executions: ~a\n" (- num-M num-fM)))
+    (display (format #f "# of tested executions: ~a\n" (- num-R num-fR)))
 		)
 	)
 )
